847eaf4e9b4e6e52d22680e31e294497102ae4da
Yifeng Zhang <yifeng2019@gmail.com>
2025-06-06 17:22:03 -0700

    rename ecr succeed in aws

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -2,6 +2,7 @@
 resource "aws_ecr_repository" "api_repository" {
   name                 = "order-processor-api"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -18,6 +19,7 @@ resource "aws_ecr_repository" "api_repository" {
 resource "aws_ecr_repository" "frontend_repository" {
   name                 = "order-processor-frontend"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
