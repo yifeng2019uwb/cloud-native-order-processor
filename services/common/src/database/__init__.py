ed1507683f86398362d69e095e3cd9f0ee07d1aa
Yifeng Zhang <yifeng2019@gmail.com>
2025-07-08 18:53:31 -0700

    clear up

--- services/common/src/database/__init__.py
+++ services/common/src/database/__init__.py
@@ -1,6 +1,4 @@
 # services/common/database/__init__.py
-from .dynamodb_service import DynamoDBManager, get_dynamodb
-from .dynamodb_service import OrderService, InventoryService
 
 __all__ = [
     "DynamoDBManager",
