079a9d12f0d52fa0c09f3b961c5ac2b7adb475f6
Yifeng Zhang <yifeng2019@gmail.com>
2025-07-15 15:06:58 -0700

    fetch data from coingecko

--- services/inventory_service/src/data/__init__.py
+++ services/inventory_service/src/data/__init__.py
@@ -4,16 +4,10 @@ Data initialization package for inventory service
 
 from .init_inventory import (
     initialize_inventory_data,
-    startup_inventory_initialization,
-    get_inventory_summary,
-    check_if_data_exists,
-    create_sample_assets
+    startup_inventory_initialization
 )
 
 __all__ = [
     "initialize_inventory_data",
-    "startup_inventory_initialization",
-    "get_inventory_summary",
-    "check_if_data_exists",
-    "create_sample_assets"
+    "startup_inventory_initialization"
 ]
