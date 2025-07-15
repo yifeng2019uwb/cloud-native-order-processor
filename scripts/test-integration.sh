010395ea38b721f8ef504b3af743c14d0a72729c
Yifeng Zhang <yifeng2019@gmail.com>
2025-07-14 17:49:26 -0700

    clean up no need files

--- scripts/test-integration.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# scripts/test-integration.sh
-# Wrapper to run integration tests from the integration-tests package
-set -e
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-exec "$DIR/integration-tests/run-integration-tests.sh" "$@"
